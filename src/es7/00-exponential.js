12f3b35f69ea3684054238af0d8669e6a22b55b9
Jonathan Zamora <jonathan.zamora12@gmail.com>
2023-03-31 10:38:09 -0300

    Se agregan archivos de ecmascript7, set, exponential y array include

---
+++ src/es7/00-exponential.js
@@ -0,0 +1,3 @@
+const data = 3 ** 4;                // 3 a potencia de 4.
+console.log(data);                  // Resultado 81.
+
---
+++ src/es7/01-array-includes.js
@@ -0,0 +1,7 @@
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+
+console.log(numbers.includes(4));
+
+const list = ['Oscar', 'Davida', 'Ana'];
+
+console.log(list.includes('Oscar'));
